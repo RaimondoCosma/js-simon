e364deae50d3773453da07fd45d08e17867e2347
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-04 13:39:36 +0100

    Aggiunge array creato dinamicamente in console.log

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+/*---------------------
+    FUNCTIONS
+---------------------*/
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+  /*---------------------
+    MAIN
+---------------------*/
+let pcNumbers = [];
+while ( pcNumbers.length < 5 ){
+    let random = getRndInteger(1, 100)
+    if ( !pcNumbers.includes(random) ){
+        pcNumbers.push(random);
+    }
+}
+console.log(pcNumbers);
