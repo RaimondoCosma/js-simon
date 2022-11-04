6b870bcb29926a1500dcbeb6601f66a575a2de4a
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-04 13:56:58 +0100

    Stampa array nel DOM

--- js/main.js
+++ js/main.js
@@ -9,11 +9,18 @@ function getRndInteger(min, max) {
   /*---------------------
     MAIN
 ---------------------*/
+// Dichiaro funzione relativa al #random-number del mio DOM
+const randomNumbers = document.getElementById('random-number');
+
 let pcNumbers = [];
 while ( pcNumbers.length < 5 ){
     let random = getRndInteger(1, 100)
     if ( !pcNumbers.includes(random) ){
         pcNumbers.push(random);
     }
+    let numberItem = document.createElement('div');
+    numberItem.classList.add('box__number')
+    randomNumbers.append(numberItem);
+    numberItem.append(random);
 }
 console.log(pcNumbers);
